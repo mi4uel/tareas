e3d367d1672047eb26a65999d73986241bae91bf
mi4uel <you@example.com>
2021-07-14 18:34:26 -0300

    actualizamos

--- js/app.js
+++ js/app.js
@@ -1,25 +1,49 @@
 const app = new Vue({
-    el:'section',
+    el:'#container',
     data:{
         tasks:[
             {nombre:'compras', descripcion:'ir al super',estado:'a realizar'},
-            {nombre:'aprobar', descripcion:'estudiar',estado:'a realizar'}
+            {nombre:'aprobar', descripcion:'estudiar',estado:'en proceso'}
             ],
         nombre:'',
-        descripcion:''
+        descripcion:'',
+        editado:null,
+        estadosDisponoble:['a realizar','en proceso','finalizado']
         },        
     methods:{
         agregarTarea(){
             if(this.nombre.length==0 || this.descripcion.length==0) return;
-            this.tasks.push({
-                nombre: this.nombre,
-                descripcion: this.descripcion,
-                estado:'a realizar'
-            }),
-            console.log(this.newTask),
-            this.nombre='',
-            this.descripcion=''       
+            if(this.editado===null){
+                this.tasks.push({
+                    nombre: this.nombre,
+                    descripcion: this.descripcion,
+                    estado:'a realizar'
+                })
+                this.nombre='',
+                this.descripcion=''
+            }else{
+                this.tasks[this.editado].nombre = this.nombre,
+                this.tasks[this.editado].descripcion = this.descripcion,
+                this.editado = null,
+                this.nombre='',
+                this.descripcion=''
+            }
+        },
+        borrar(index){
+            this.tasks.splice(index, 1)
+        },
+        editar(index){
+            this.nombre = this.tasks[index].nombre,
+            this.descripcion = this.tasks[index].descripcion,
+            this.editado= index
+        },
+        estado(index){
+            let newIndex = this.estadosDisponoble.indexOf(this.tasks[index].estado);
+            if(++newIndex > 2) newIndex=0;
+            this.tasks[index].estado = this.estadosDisponoble[newIndex]
+
         }
     }
 });
+
 
