925ada5f097204d5c66c08acb1ed99a30c859152
mi4uel <you@example.com>
2021-07-16 17:40:53 -0300

    actualizamos todo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el:'#container',
     data:{
+        buscar:null,
         tasks:[
             {nombre:'compras', descripcion:'ir al super',estado:'a realizar'},
             {nombre:'aprobar', descripcion:'estudiar',estado:'en proceso'}
@@ -9,7 +10,8 @@ const app = new Vue({
         descripcion:'',
         editado:null,
         estadosDisponoble:['a realizar','en proceso','finalizado']
-        },        
+        },
+                
     methods:{
         agregarTarea(){
             if(this.nombre.length==0 || this.descripcion.length==0) return;
@@ -42,6 +44,17 @@ const app = new Vue({
             if(++newIndex > 2) newIndex=0;
             this.tasks[index].estado = this.estadosDisponoble[newIndex]
 
+        }
+    },
+    computed:{
+        busqueda(){
+            if(this.buscar===null){
+                return this.tasks
+            }
+            else{           
+             return this.tasks.filter((task)=> 
+             task.nombre.includes(this.buscar) || task.descripcion.includes(this.buscar));
+            }
         }
     }
 });
